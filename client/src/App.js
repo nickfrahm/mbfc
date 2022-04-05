a9a5dea4250a72d4ff7ebdeab7b510503eda573e
nickfrahm <nfrahm24@gmail.com>
2022-04-04 20:00:18 -0500

    Set up a test route to make sure react and express were working together. Set up npm scripts too

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [test, setTest] = useState('');
+  useEffect(() => {
+    fetch('/api/test')
+      .then((res) => res.json())
+      .then((txtObj) => setTest(txtObj.text))
+      .catch((err) => {
+        setTest('something went wrong');
+        console.log(err);
+      });
+  }, []);
+  return <div className='App'>{test}</div>;
 }
 
 export default App;
