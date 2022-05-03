c6ed84028f55238fc51b0480e00793af8ebce236
nickfrahm <nfrahm24@gmail.com>
2022-05-02 20:08:12 -0500

    set up .env for db, added barebones error handling for routes, conncected to mongo

---
+++ models/player.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PlayerSchema = new Schema({
+  name: { type: String, required: true },
+  teams: { type: [String], required: true },
+  uclPoints: { type: Number, required: true },
+  competitionWins: { type: [String], required: true },
+});
+
+module.exports = PlayerSchema;
--- server.js
+++ server.js
@@ -2,13 +2,39 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mbfcAPI = require('./routes/api/tables/tableData');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+//Connect to DB
+mongoose.connect(process.env.DB_CON_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+setTimeout(() => {
+  console.log(mongoose.connection.readyState);
+}, 10000);
+
+//Get Routes For API
 app.use('/api', mbfcAPI);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Retrieve data from /api/ routes');
+});
+
+//error handling
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(404);
+  res.send('error finding route');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
